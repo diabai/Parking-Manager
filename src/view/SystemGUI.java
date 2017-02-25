cb22b19dc40fd49e3efb4096b619896d8f567b09
Lutetium15 <DemaPavlovskiy@gmail.com>
2017-02-25 15:43:04 -0800

    Changed layout a little. Added a view button.

--- src/view/SystemGUI.java
+++ src/view/SystemGUI.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,26 +15,61 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 
-public class SystemGUI extends JFrame implements ActionListener {
+import model.Employee;
+import model.SystemDB;
+
+public class SystemGUI extends JFrame implements ActionListener, TableModelListener {
+    
+    private SystemDB db;
+    private List<Employee> employeeList;
+    private Object[][] currTableData;
 
     /** For Serialization.*/
     private static final long serialVersionUID = 6515753873615102989L;
     
+
+    
     /** Buttons for reserving parking spots and updating employees. */
-    private JButton resVisitorBtn, resEmployeeBtn, updateEmployeeBtn, addParkingLot, addEmployee, addParkingSpace;
+    private JButton viewEmployeesButton, resVisitorBtn, resEmployeeBtn, updateEmployeeBtn, addParkingLot, addEmployee, addParkingSpace;
     
     /** Table that contains information from database. */
     private JTable staffTable, parkingSpotTable;
     
     /** Main panels that divide the GUI window. */
-    private JPanel upperPnl, mainPnl, staffTblePnl, pkSpotTblPnl;
+    private JPanel upperPnl, pnlContent, staffTblePnl, pkSpotTblPnl;
 
     public SystemGUI()  {
         super("ParkingApplication");
+        
+        updateEmployeeTable();       
         createComponents();
         setVisible(true);
-        setMinimumSize(new Dimension(500, 500));
+        setMinimumSize(new Dimension(700, 600));
+    }
+    
+    /** Updates current list of employees and updates the currentTable shown. */
+    private void updateEmployeeTable() {
+         db = new SystemDB();
+        try
+        {
+            employeeList = db.getEmployees();
+            
+            currTableData = new Object[employeeList.size()][4];
+            for (int i=0; i<employeeList.size(); i++) {
+                currTableData[i][0] = employeeList.get(i).getmEmpNumber();
+                currTableData[i][1] = employeeList.get(i).getmName();
+                currTableData[i][2] = employeeList.get(i).getmExtNum();
+                currTableData[i][3] = employeeList.get(i).getmVehicleLicense();
+                
+            }
+            
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
     }
     
     /** Creates the GUI components. */
@@ -40,10 +77,11 @@ public class SystemGUI extends JFrame implements ActionListener {
         
         // Lay out table into two sections
         upperPnl = createUpperPanel();
-        mainPnl = createStaffPanel();
+        pnlContent = new JPanel();
+        pnlContent.add(createEmployeeTablePanel());
         
         this.add(upperPnl, BorderLayout.NORTH);
-        this.add(mainPnl, BorderLayout.CENTER);
+        this.add(pnlContent, BorderLayout.CENTER);
                  
     }
     
@@ -51,55 +89,64 @@ public class SystemGUI extends JFrame implements ActionListener {
     private JPanel createUpperPanel() {
         
         upperPnl = new JPanel();
+        JPanel buttonPanel1 = new JPanel(new GridLayout(1, 4, 5, 5));
+        JPanel buttonPanel2 = new JPanel(new GridLayout(1, 3, 5, 5));
         
-        //settings its layout to have a capacity of 2 rows and 3 colums
+        //settings its layout to have a capacity of 2 rows and 3 columns
         // 5 is the space between the buttons
-        upperPnl.setLayout(new GridLayout(2,3,5,5));
+        upperPnl.setLayout(new GridLayout(2,1,5,5));
+        
+        viewEmployeesButton = new JButton("View Employees");
+        viewEmployeesButton.addActionListener(this);
+        buttonPanel1.add(viewEmployeesButton);
         
         // Reserve Visitor Parking Spot Button
         resVisitorBtn = new JButton("Reserve Visitor Parking");
         resVisitorBtn.addActionListener(this);
-        upperPnl.add(resVisitorBtn);
+        buttonPanel1.add(resVisitorBtn);
         
         // Employee Visitor Parking Spot Button
         resEmployeeBtn = new JButton("Reserve Employee Parking");
         resEmployeeBtn.addActionListener(this);
-        upperPnl.add(resEmployeeBtn);
+        buttonPanel1.add(resEmployeeBtn);
         
         // Update Employee information Button
         updateEmployeeBtn = new JButton("Update Employee");
         updateEmployeeBtn.addActionListener(this);
-        upperPnl.add(updateEmployeeBtn);
+        buttonPanel1.add(updateEmployeeBtn);
         
         //Add parking lot button
         
         addParkingLot = new JButton("Add parking lot");
         addParkingLot.addActionListener(this);
-        upperPnl.add(addParkingLot);
+        buttonPanel2.add(addParkingLot);
         
         //Add employee
         addEmployee = new JButton("Add employee");
         addEmployee.addActionListener(this);
-        upperPnl.add(addEmployee);
+        buttonPanel2.add(addEmployee);
         
         //Add parking space
         addParkingSpace = new JButton("Add parking space");
         addParkingSpace.addActionListener(this);
-        upperPnl.add(addParkingSpace);
+        buttonPanel2.add(addParkingSpace);
+        
+        upperPnl.add(buttonPanel1);
+        upperPnl.add(buttonPanel2);
         
         return upperPnl;
     }
     
     /** Creates panel with table of staff. */
-    private JPanel createStaffPanel() {
-        // List panel - blatantly ripped from Professor's code
-        staffTblePnl = new JPanel();
-        staffTable = new JTable(); // NEEDS DATA
+    private JPanel createEmployeeTablePanel() {
+        String staffColumnNames[] = {"Employee Number", "Employee Name", "Extension Number", "Vehicle License Number"};
+        JPanel pnl = new JPanel();
+        staffTable = new JTable(currTableData, staffColumnNames);
         JScrollPane scrollPane = new JScrollPane(staffTable);
-        staffTblePnl.add(scrollPane);
-        //staffTable.getModel().addTableModelListener(this);
+        pnl.add(scrollPane);
+        staffTable.getModel().addTableModelListener(this);
         
-        return staffTblePnl;
+        return pnl;
     }
     
     /** Shows the panel needed for reserving a parking spot for visitors. */
@@ -141,9 +188,9 @@ public class SystemGUI extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == resVisitorBtn) 
         {
-            mainPnl.removeAll();
-            mainPnl.add(createResVisitorPnl());
-            mainPnl.revalidate();
+            pnlContent.removeAll();
+            pnlContent.add(createResVisitorPnl());
+            pnlContent.revalidate();
             this.repaint();
         }
         else if (e.getSource() == resEmployeeBtn)
@@ -163,7 +210,21 @@ public class SystemGUI extends JFrame implements ActionListener {
         } else if (e.getSource() == addParkingSpace) {
         	
         }
+        else if (e.getSource() == viewEmployeesButton) 
+        {
+            pnlContent.removeAll();
+            updateEmployeeTable();
+            pnlContent.add(createEmployeeTablePanel());
+            pnlContent.revalidate();
+            this.repaint();
+        }
 
+    }
+
+    @Override
+    public void tableChanged(TableModelEvent arg0) {
+        // TODO Auto-generated method stub
+        
     }
     
     
